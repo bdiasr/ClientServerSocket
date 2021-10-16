f3c59814f7e6971ea48dc6f96ace7ad9bf4ecc1f
julianabfreitas <julianabfreitas@usp.br>
2021-10-16 13:06:14 -0300

    adicionando struct ips

--- code/connect.h
+++ code/connect.h
@@ -1,20 +1,20 @@
-
 struct in_addr{};
 
-struct sockaddr_in
-{
+struct sockaddr_in{
     //Familia do endereço
     short int sin_family;
-
     //número da porta
     unsigned short int sin_port; 
-
     //IP do HOST
     struct in_addr sin_addr;
-
-    /*
-    Zera a estrutura, algum espaço com struct sockaddr
-    */
+    //Zera a estrutura, algum espaço com struct sockaddr
     unsigned char sin_zero[8];
-    
 };
+
+struct ips{
+    char serverIP[15] = "000.000.00.0";
+    char clientIP[15] = "000.000.00.0";
+};
+
+
+
